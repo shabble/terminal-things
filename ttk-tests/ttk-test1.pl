4c2ce8bb45d56c9f72b18b10cd13d30c5a9113e7
Tom Feist <shabble@metavore.org>
2011-05-06 00:37:14 +0100

    added a bunch of details to test1.  Keeps dying on get_keyname() though. Strange

--- ttk-tests/ttk-test1.pl
+++ ttk-tests/ttk-test1.pl
@@ -3,6 +3,7 @@
 use strict;
 use warnings;
 use feature qw/say switch/;
+use Data::Dumper;
 
 use Term::TermKey;
 
@@ -15,4 +16,47 @@ my $key;
 while((my $ret = $tk->waitkey($key)) != RES_EOF) {
 
     say "Got key: " . $tk->format_key($key, FORMAT_VIM);
+
+    say "Key Details:";
+
+    process_key($key);
+    
+    say "-" x 30;
+}
+
+
+sub process_key {
+    my ($key) = @_;
+    my $type = $key->type;
+
+    given ($type) {
+        when (TYPE_UNICODE) {
+            say "Unicode value is: " . $key->codepoint;
+        }
+        when (TYPE_FUNCTION) {
+            say "F-Key number is: " . $key->number;
+        }
+        when (TYPE_KEYSYM) {
+            say "KeySym is: " . sprintf("0x%04x", $key->sym) .
+              " which is " . (Term::TermKey->get_keyname($key->sym));
+        }
+        when (TYPE_MOUSE) {
+            say "Mouse is: magic!";
+        }
+    }
+    say "UTF Value is: " . $key->utf8;
+
+    my $mod = $key->modifiers;
+
+    given ($mod) {
+        when (KEYMOD_SHIFT) {
+            say "With shift";
+        }
+        when (KEYMOD_ALT) {
+            say "with alt";
+        }
+        when (KEYMOD_CTRL) {
+            say "with ctrl";
+       }
+    }
 }
